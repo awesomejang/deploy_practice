a736c3e612496dbdd0f315fe1fce7238e82731bb
awesomejang <uljang1995@gmail.com>
2024-10-31 00:58:26 +0900

    java 버전 변경
    22 -> 17

--- src/main/java/com/deploytest/controller/HomeController.java
+++ src/main/java/com/deploytest/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.deploytest.controller;
 
 import com.deploytest.dto.CommonResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("")
     public ResponseEntity<CommonResponseDto<Void>> home() {
+        logger.info("Request to Home");
         return ResponseEntity.ok(CommonResponseDto.success());
     }
 
     @GetMapping("health-check")
     public ResponseEntity<CommonResponseDto<Void>> healthCheck() {
+        logger.info("Health Check");
         return ResponseEntity.ok(CommonResponseDto.success());
     }
 }
