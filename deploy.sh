b2e6d66f1de2e52b0dddf5066c27b334ea870907
awesomejang <uljang1995@gmail.com>
2024-11-21 01:03:27 +0900

    deploy 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 프로젝트 경로 설정
-PROJECT_DIR="/opt/repositories/deploy_practice"
+PROJECT_DIR="/opt/servers/deploy_practice:8080"
 BRANCH="main"
 JAR_NAME="deploy-test-0.0.1.jar" # 실제 jar 이름으로 변경
 PORT=8080
@@ -10,8 +10,8 @@ PORT=8080
 set -e
 
 # 1. 프로젝트 디렉토리로 이동
-echo "Navigating to project directory: $PROJECT_DIR"
-cd $PROJECT_DIR || { echo "Project directory not found! Exiting."; exit 1; }
+#echo "Navigating to project directory: $PROJECT_DIR"
+#cd $PROJECT_DIR || { echo "Project directory not found! Exiting."; exit 1; }
 
 # 2. Git Pull
 echo "Pulling latest changes from $BRANCH..."
@@ -26,6 +26,20 @@ echo "Stopping any existing Gradle daemons..."
 # 3. Build
 echo "Building the project..."
 ./gradlew clean bootJar # Gradle 사용 시
+
+BUILD_JAR_FILE=$(ls ./build/libs/deploy-test-*.jar)
+echo "build jar file name : $BUILD_JAR_FILE"
+
+SYMBOLIC_LINK="$PROJECT_DIR/current.jar"
+
+echo "Copying jar file to the upper directory..."
+cp "$BUILD_JAR_FILE" "$PROJECT_DIR/"
+
+
+TARGET_JAR_FILE=$(basename $BUILD_JAR_FILE)
+
+echo "Link symbol with $TARGET_JAR_FILE..."
+ln -sf "$TARGET_JAR_FILE" "$SYMBOLIC_LINK"
 
 # 4. Kill existing process on port 8080
 echo "Finding process using port $PORT..."
@@ -55,6 +69,7 @@ fi
 
 # 6. 애플리케이션 실행
 echo "Starting new application on port $PORT..."
-nohup java -jar -Dserver.port=$PORT build/libs/$JAR_NAME --spring.profiles.active=prod > app.log 2>&1 &
+nohup java -jar -Dserver.port=$PORT "$SYMBOLIC_LINK" --spring.profiles.active=prod > app.log 2>&1 &
+#nohup java -jar -Dserver.port=$PORT "$SYMBOLIC_LINK" --spring.profiles.active=prod > app.log 2>&1 &
 
 echo "Deployment complete!"
